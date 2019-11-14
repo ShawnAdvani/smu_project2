81085594fce3a520e1844627e908c9d02bfdcdbd
PedroSF88 <flo.pedro.sal@outlook.com>
2019-11-13 19:23:24 -0600

    updated schema

--- table_schema.sql
+++ table_schema.sql
@@ -4,146 +4,130 @@
 
 
 CREATE TABLE "2BR" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_2BR" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "3BR" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_3BR" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "4BR" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_4BR" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "5BR" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_5BR" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "Condo" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_Condo" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "dup_tri" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_dup_tri" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "Multi" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_Multi" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "Manufactured" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_Manufactured" PRIMARY KEY (
         "index"
      )
 );
 
 CREATE TABLE "SFR" (
-    "index" int   NOT NULL,
-    "RegionName" varchar   NOT NULL,
-    "State" varchar   NOT NULL,
-    "CountyName" varchar   NOT NULL,
-    "Sale_TTM_Avg" int   NOT NULL,
-    "TTM_Avg" int   NOT NULL,
-    "Year_Rent_%_of_Sale" int   NOT NULL,
+    "index" int,
+    "RegionName" varchar,
+    "State" varchar,
+    "CountyName" varchar,
+    "Sale_TTM_Avg" float,
+    "TTM_Avg" float,
+    "Year_Rent_Percent_of_Sale" float,
     CONSTRAINT "pk_SFR" PRIMARY KEY (
         "index"
      )
 );
 
-ALTER TABLE "2BR" ADD CONSTRAINT "fk_2BR_index" FOREIGN KEY("index")
-REFERENCES "3BR" ("index");
-
-ALTER TABLE "3BR" ADD CONSTRAINT "fk_3BR_index" FOREIGN KEY("index")
-REFERENCES "4BR" ("index");
-
-ALTER TABLE "4BR" ADD CONSTRAINT "fk_4BR_index" FOREIGN KEY("index")
-REFERENCES "5BR" ("index");
-
-ALTER TABLE "5BR" ADD CONSTRAINT "fk_5BR_index" FOREIGN KEY("index")
-REFERENCES "Condo" ("index");
-
-ALTER TABLE "Condo" ADD CONSTRAINT "fk_Condo_index" FOREIGN KEY("index")
-REFERENCES "dup_tri" ("index");
-
-ALTER TABLE "dup_tri" ADD CONSTRAINT "fk_dup_tri_index" FOREIGN KEY("index")
-REFERENCES "Multi" ("index");
-
-ALTER TABLE "Multi" ADD CONSTRAINT "fk_Multi_index" FOREIGN KEY("index")
-REFERENCES "Manufactured" ("index");
-
-ALTER TABLE "Manufactured" ADD CONSTRAINT "fk_Manufactured_index" FOREIGN KEY("index")
-REFERENCES "SFR" ("index");
-
-ALTER TABLE "SFR" ADD CONSTRAINT "fk_SFR_index" FOREIGN KEY("index")
-REFERENCES "2BR" ("index");
-
+CREATE TABLE "CityLatLong" (
+   "'index'" int   NOT NULL,
+   "'RegionName'" varchar   NOT NULL,
+   "'State'" varchar   NOT NULL,
+   "'Latitude'" float,
+   "'Longitude'" float,
+   "'CountyName'" varchar,
+   CONSTRAINT "pk_CityLatLong" PRIMARY KEY (
+       "'index'"
+    )
+);
